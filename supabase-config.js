52e34658854f50586bebfefc0503c610eec1be14
leomohan <leomohan@yahoo.com>
2026-04-23 16:19:55 +0400

    U

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
-export const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+export const SUPABASE_URL = "https://wigtjjchoqoybgzkikec.supabase.co";
+export const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEYeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpZ3RqamNob3FveWJnemtpa2VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5MjM0MzcsImV4cCI6MjA5MjQ5OTQzN30.WIuVenRx0h_sFiyTCsoSgCoDY5iMdA6AhIl2LOgGjbE";
 
 // The anon key is safe to use in a browser app.
 // Do not place the service_role key in this file.
